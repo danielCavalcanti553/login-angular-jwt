873f44351e3fb98a80900c29a63608e1e5cabdd9
daniel <daniel.cavalcanti@outlook.com.br>
2020-07-13 14:18:17 -0300

    insert server

--- server_php_jwt/app_client.php
+++ server_php_jwt/app_client.php
@@ -7,12 +7,14 @@ $request = json_decode($postdata);
 
 if (($username == 'admin@email.com') && ($password == '123456')) {
     require_once('jwt.php');
-    $userId = 'USER123';
+    $userId = 'admin@email.com';
 
     $serverKey = '123456789abcdefg123456789abcdefg';
     
     $tArray = array();
     $tArray['userId'] = $userId;
+    $nbf = time();
+    $exp = $nbf + 60*5;
     if (isset($nbf)) {$tArray['nbf'] = $nbf;}
     if (isset($exp)) {$tArray['exp'] = $exp;}
     $token = JWT::encode($tArray, $serverKey);
@@ -26,8 +28,9 @@ if (($username == 'admin@email.com') && ($password == '123456')) {
     echo $jsonReturn;
 
 }else {
+    $t =  time() + 60*5;
     $returnArray = array("message" =>
-    array('code' => 203,'text' => 'Login invalido'));
+    array('code' => 203,'text' => 'Login invalido: '.$t.'-'.time()));
     $jsonReturn = json_encode($returnArray, JSON_PRETTY_PRINT);
     header('Content-type: application/json');
     echo $jsonReturn;
